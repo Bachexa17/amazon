9d50ecd1b97cdb266f29f6e3f7ec66e8bb6b5bdf
bachexa17 <bacho.begiashvili@gmail.com>
2024-09-13 22:50:49 -0800

    changed product button styles

--- scripts/main.js
+++ scripts/main.js
@@ -13,26 +13,28 @@ function renderHTML() {
     html += `
     <div class="product">
       <img class="product-img" src="${product.image}">
-      <span class="product-name">${product.name}</span>
-      <div class="product-rating">
-        <span class="rating">${product.ratings.ratingFloat}</span>
-        <span class="amount">${product.ratings.amount}</span>
+      <div class="product-info">
+        <span class="product-name">${product.name} ${product.name} ${product.name}</span>
+        <div class="product-rating">
+          <span class="rating">${product.ratings.ratingFloat}</span>
+          <span class="amount">${product.ratings.amount}</span>
+        </div>
+        <span class="product-price">$${convertCent(product.priceCent)}</span>
+        <div class="product-buttons">
+          <select class="quantity-selector">
+            <option selected="" value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+          </select>
+          <button class="add-cart-button">Add to Cart</span>
+        </div>
       </div>
-      <span class="product-price">$${convertCent(product.priceCent)}</span>
-      <div class="product-buttons">
-        <select class="quantity-selector">
-          <option selected="" value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-          <option value="6">6</option>
-          <option value="7">7</option>
-          <option value="8">8</option>
-          <option value="9">9</option>
-        </select>
-        <button class="add-cart-button">Add to Cart</span>
-      </div>  
     </div>
     `;
   });
